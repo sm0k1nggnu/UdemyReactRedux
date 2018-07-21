2dbce741c94e29ba248426f485d4c6a4121dec1e
David May <me@davidmay.de>
2018-07-21 17:40:20 +0200

    controls + dynamic components

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './BuildControls.css'
+
+const buildControls = ( props ) => (
+    <div>skslk</div>
+);
+
+export default buildControls;
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            meat: 1,
+            cheese: 0,
+            bacon: 0
+        }
+    }
     render() {
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Building Tools</div>
             </Aux>
         )
