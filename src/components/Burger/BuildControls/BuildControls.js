2d7d2a1b986ea12ed96393b1180080b25f6571d7
David May <me@davidmay.de>
2018-07-30 22:53:56 +0200

    remove items, disable button if zero, show price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,12 +11,15 @@ const controls = [
 
 const buildControls = ( props ) => (
     <div className={classes.BuildControls}>
+        <p>Price: {props.price.toFixed(2)}</p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
                 type={ctrl.type}
                 added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
             />
         ))}
     </div>
